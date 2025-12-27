7aa11068d157909031c2734699c77c8012de1710
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-28 00:10:53 +0700

    Add loadConfigurationFile method

--- src/main/java/org/swan/httpserver/config/ConfigurationManager.java
+++ src/main/java/org/swan/httpserver/config/ConfigurationManager.java
@@ -1,5 +1,11 @@
 package org.swan.httpserver.config;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.swan.httpserver.util.Json;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConfigurationManager {
     private static ConfigurationManager myconfigurationManager;
     private static Configuration myCurrentConfiguration;
@@ -15,8 +21,15 @@ public class ConfigurationManager {
     /*
     User to load a configuration file by the path provided
      */
-    public void loadConfigurationFile(String filePath) {
-
+    public void loadConfigurationFile(String filePath) throws IOException {
+        FileReader fileReader = new FileReader(filePath);
+        StringBuffer stringBuffer = new StringBuffer();
+        int i;
+        while ((i = fileReader.read()) != -1) {
+            stringBuffer.append((char)i);
+        }
+        JsonNode conf = Json.parse(stringBuffer.toString());
+        myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
     }
 
     /*
